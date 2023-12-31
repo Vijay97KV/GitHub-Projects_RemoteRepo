3a8925d3c2a852b6e8c2bde03cdc6c83af640a0b
VijayKV <vijay97kcv@gmail.com>
2023-12-31 13:25:04 +0530

    L10N Test plans

---
+++ src/test/java/localizationTesting/L10NTesting.java
@@ -0,0 +1,17 @@
+package localizationTesting;
+
+import org.testng.annotations.Test;
+
+public class L10NTesting {
+	
+	@Test
+	public void time() {
+		System.out.println("American Time Zone");	
+	}
+	@Test
+	public void language() {
+		System.out.println("US-English");
+		
+	}
+
+}
